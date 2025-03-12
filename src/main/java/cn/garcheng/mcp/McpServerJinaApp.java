01114f58200944f297068f69252d908f74e80e4e
ZhongJiaCheng <zhongjiacheng@jzdrones.com>
2025-03-12 16:57:35 +0800

    feat: project init.

---
+++ src/main/java/cn/garcheng/mcp/McpServerJinaApp.java
@@ -0,0 +1,25 @@
+package cn.garcheng.mcp;
+
+import cn.garcheng.mcp.service.JinaService;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * start
+ */
+
+@SpringBootApplication
+public class McpServerJinaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpServerJinaApp.class, args);
+    }
+
+    @Bean
+    public ToolCallbackProvider jinaSearchTools(JinaService jinaService) {
+        return MethodToolCallbackProvider.builder().toolObjects(jinaService).build();
+    }
+}
